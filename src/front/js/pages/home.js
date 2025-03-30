f6016d5937ff8b1e5e6bfe41ceabdcb32b4ee2a9
Ender Calderón <177250779+Yohebyth@users.noreply.github.com>
2025-03-30 22:36:39 +0000

    home ok

--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,7 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
 import "../../styles/home.css";
+import { Link } from "react-router-dom";
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
@@ -8,6 +9,10 @@ export const Home = () => {
 	return (
 		<div className="text-center mt-5">
 			<h1>Hello!</h1>
+			<div className="homebuttons">
+				<Link to="/login" className="btn btn-dark home-button">login</Link>
+				<Link to="/signup" className="btn btn-dark home-button">Signup</Link>
+			</div>
 			<p>
 				<img className="Homeimg"  src="https://fi-hatchbox-production-active-storage-blobs.s3.amazonaws.com/vh4xbi6vccxxnh7y2jhuq919bwft" />
 			</p>
